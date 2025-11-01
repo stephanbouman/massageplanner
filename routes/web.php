cbb3aaa47831d6910543197bc455b7180ac200b0
Stephan Bouman <stephan@creativecycle.nl>
2025-11-01 23:48:27 +0100

    Removed routes to dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Appointment\EditAppointmentController;
 use App\Http\Controllers\Appointment\ShowAppointmentController;
 use App\Http\Controllers\Appointment\StoreAppointmentController;
 use App\Http\Controllers\Appointment\UpdateAppointmentController;
+use App\Http\Controllers\Client\Comment\DeleteCommentController;
+use App\Http\Controllers\Client\Comment\StoreCommentController;
+use App\Http\Controllers\Client\Comment\UpdateCommentController;
 use App\Http\Controllers\Client\CreateClientController;
 use App\Http\Controllers\Client\EditClientController;
 use App\Http\Controllers\Client\ListClientController;
@@ -13,21 +16,14 @@ use App\Http\Controllers\Client\StoreClientController;
 use App\Http\Controllers\Client\UpdateClientController;
 use App\Http\Controllers\ListAppointmentController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return to_route('clients.index');
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return to_route('clients.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -41,6 +37,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/clients/{client}', ShowClientController::class)->name('clients.show');
     Route::get('/clients/{client}/edit', EditClientController::class)->name('clients.edit');
     Route::patch('/clients/{client}/edit', UpdateClientController::class)->name('clients.update');
+
+    Route::post('/clients/{client}/comment', StoreCommentController::class)->name('clients.comment.store');
+    Route::patch('/clients/{client}/comment/{comment}', UpdateCommentController::class)->name('clients.comment.update');
+    Route::delete('/clients/{client}/comment/{comment}', DeleteCommentController::class)->name('clients.comment.delete');
 
     Route::get('/clients/{client}/appointment/create', CreateAppointmentController::class)->name('appointment.create');
     Route::post('/clients/{client}/appointment', StoreAppointmentController::class)->name('appointment.store');
